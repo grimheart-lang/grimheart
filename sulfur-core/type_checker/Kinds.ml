2921883eb7c3c6cd6cc3da3cafe2416264bdc174
sjpgarcia <91515796+sjpgarcia@users.noreply.github.com>
2022-01-14 22:30:47 +0800

    Implement kind unification rule

--- sulfur-core/type_checker/Kinds.ml
+++ sulfur-core/type_checker/Kinds.ml
@@ -6,9 +6,10 @@
 
    https://arxiv.org/pdf/1911.06153.pdf *)
 open Sulfur_ast
+open Sulfur_errors.Let
 
-let instantiate (_gamma : Context.t) ((_T, _K) : Type.t * Type.t) (_K : Type.t)
-    : (Context.t * Type.t, Sulfur_errors.t) result =
+let rec instantiate (_gamma : Context.t) ((_T, _K) : Type.t * Type.t)
+    (_K : Type.t) : (Context.t * Type.t, Sulfur_errors.t) result =
   failwith "instantiate: undefined"
 
 and check (_gamma : Context.t) (_T : Type.t) (_K : Type.t) :
@@ -27,10 +28,46 @@ and infer_elaborated (_gamma : Context.t) (_T : Type.t) :
     (Context.t * Type.t, Sulfur_errors.t) result =
   failwith "infer_elaborated: undefined"
 
-and unify (_gamma : Context.t) (_A : Type.t) (_B : Type.t) :
+and unify (ctx : Context.t) (_A : Type.t) (_B : Type.t) :
     (Context.t, Sulfur_errors.t) result =
-  failwith "unify: undefined"
+  match (_A, _B) with
+  (* A-U-APP *)
+  | Apply (p1, p2), Apply (p3, p4) ->
+      let* gamma = unify ctx p1 p3 in
+      unify gamma (Context.apply gamma p2) (Context.apply gamma p4)
+  (* A-U-KAPP *)
+  | KindApply (p1, p2), KindApply (p3, p4) ->
+      let* gamma = unify ctx p1 p3 in
+      unify gamma (Context.apply gamma p2) (Context.apply gamma p4)
+  (* A-U-REFL-TT *)
+  | _A, _B when Type.equal _A _B -> Ok ctx
+  (* A-U-KVARL-TT *)
+  | Unsolved a, p1 -> unify_unsolved ctx a p1
+  (* A-U-KVARR-TT *)
+  | p1, Unsolved a -> unify_unsolved ctx a p1
+  (* FALLTHROUGH *)
+  | _ -> failwith "unify: FALLTHROUGH: todo: raise a more appropriate error"
 
 and promote (_gamma : Context.t) (_a : string) (_T : Type.t) :
     (Context.t * Type.t, Sulfur_errors.t) result =
   failwith "promote: undefined"
+
+and unify_unsolved (ctx : Context.t) (a : string) (p1 : Type.t) :
+    (Context.t, Sulfur_errors.t) result =
+  let* ctx, p2 = promote ctx a p1 in
+  let* ctx1, w1, ctx2 =
+    let rec aux (l : Context.t) :
+        Context.t -> (Context.t * Type.t * Context.t, Sulfur_errors.t) result =
+      function
+      | [] -> failwith "unify_unsolved: todo: raise a more appropriate error"
+      | h :: t -> (
+          match h with
+          | Context.Element.Unsolved a' when String.equal a a' ->
+              Ok (List.rev l, failwith "todo: add kinds to unsolved elements", t)
+          | _ -> aux (h :: l) t)
+    in
+    aux [] ctx
+  in
+  let* ctx1, w2 = infer_elaborated ctx1 p2 in
+  let* ctx3 = unify ctx1 (Context.apply ctx1 w1) w2 in
+  Ok (List.append ctx3 (Context.Element.Solved (a, p2) :: ctx2))
