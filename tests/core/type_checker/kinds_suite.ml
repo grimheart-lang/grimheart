712de033f2f2528a5512159c069748b0b0b38175
sjpgarcia <91515796+sjpgarcia@users.noreply.github.com>
2022-01-21 00:28:15 +0800

    Add more unit tests for kind checker

--- tests/core/type_checker/kinds_suite.ml
+++ tests/core/type_checker/kinds_suite.ml
@@ -45,8 +45,17 @@ module Test_input : TEST_INPUT = struct
         ~data:
           (forall' "a" t_type
           @@ fn (forall "b" @@ fn (var "b") (var "a")) (var "a"));
+      Hashtbl.add_exn environment ~key:"Escape'"
+        ~data:
+          (forall' "a" t_type
+          @@ fn (forall' "b" t_type @@ fn (var "b") (var "a")) (var "a"));
+      Hashtbl.add_exn environment ~key:"HigherRank"
+        ~data:(fn (forall "a" @@ fn (var "a") (var "a")) t_type);
+      Hashtbl.add_exn environment ~key:"HigherRank'"
+        ~data:(fn (forall' "a" t_type @@ fn (var "a") (var "a")) t_type);
       Hashtbl.add_exn environment ~key:"Identity"
-        ~data:(forall' "a" t_type @@ fn (var "a") (var "a"))
+        ~data:(forall' "a" t_type @@ fn (var "a") (var "a"));
+      Hashtbl.add_exn environment ~key:"TypeToType" ~data:(fn t_int t_int)
   end
 end
 
@@ -55,9 +64,22 @@ let run () =
   let open Test_utils (Test_input) in
   run ~and_exit:false "Kind Checker Unit Tests"
     [
-      ( "infer-skolem-escapes"
+      ( "infer-higher-rank-kinds"
+      , [
+          infer_kind_test_case "higher rank kinds with polytypes"
+            (Apply (Constructor "HigherRank", Constructor "Identity"))
+        ; infer_kind_test_case "higher rank kinds with polytypes, again"
+            (Apply (Constructor "HigherRank'", Constructor "Identity"))
+        ; infer_kind_test_case "higher rank kinds with monotypes"
+            (Apply (Constructor "HigherRank", Constructor "TypeToType"))
+        ; infer_kind_test_case "higher rank kinds with monotypes, again"
+            (Apply (Constructor "HigherRank'", Constructor "TypeToType"))
+        ] )
+    ; ( "infer-skolem-escapes"
       , [
           infer_kind_fail_case "skolem escapes are caught"
             (Apply (Constructor "Escape", Constructor "Identity"))
+        ; infer_kind_fail_case "skolem escapes are caught, again"
+            (Apply (Constructor "Escape'", Constructor "Identity"))
         ] )
     ]
