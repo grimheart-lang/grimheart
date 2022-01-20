b8b08de79afe13c7c14b7711c7d336cbeffbb9f1
sjpgarcia <91515796+sjpgarcia@users.noreply.github.com>
2022-01-21 00:10:57 +0800

    Create test suite for the kind checker

---
+++ tests/core/type_checker/kinds_suite.ml
@@ -0,0 +1,63 @@
+open Core_kernel
+open Grimheart_ast
+open Grimheart_core_type_checker
+
+module type TEST_INPUT = sig
+  val context : Context.t
+
+  module Environment : Environment.S
+end
+
+module Test_utils (I : TEST_INPUT) = struct
+  module Kinds = Kinds.Make (I.Environment)
+
+  let infer_kind_test_case annotation value =
+    let check () =
+      Alcotest.(check bool)
+        annotation true
+        (Result.is_ok @@ Kinds.infer I.context value)
+    in
+    Alcotest.test_case annotation `Quick check
+
+  let infer_kind_fail_case annotation value =
+    let check () =
+      Alcotest.(check bool)
+        annotation false
+        (Result.is_ok @@ Kinds.infer I.context value)
+    in
+    Alcotest.test_case annotation `Quick check
+
+  include Type.Primitives
+  include Type.Sugar
+end
+
+module Test_input : TEST_INPUT = struct
+  open Type.Sugar
+  open Type.Primitives
+
+  let context : Context.t = [Unsolved "A"; Unsolved "B"]
+
+  module Environment = struct
+    include Environment.Make ()
+
+    let () =
+      Hashtbl.add_exn environment ~key:"Escape"
+        ~data:
+          (forall' "a" t_type
+          @@ fn (forall "b" @@ fn (var "b") (var "a")) (var "a"));
+      Hashtbl.add_exn environment ~key:"Identity"
+        ~data:(forall' "a" t_type @@ fn (var "a") (var "a"))
+  end
+end
+
+let run () =
+  let open Alcotest in
+  let open Test_utils (Test_input) in
+  run ~and_exit:false "Kind Checker Unit Tests"
+    [
+      ( "infer-skolem-escapes"
+      , [
+          infer_kind_fail_case "skolem escapes are caught"
+            (Apply (Constructor "Escape", Constructor "Identity"))
+        ] )
+    ]
